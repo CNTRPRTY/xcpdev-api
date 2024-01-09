f247a82e585d19ea6fb9bb956c999b0418daf574
Juan <dev@juanaleman.com>
2024-01-08 20:08:33 -0600

    adding quantityWithDivisibility to util
    (cherry picked from commit ef57218f9347bb5d25152921819e25733699d8ae)

--- src/util.js
+++ src/util.js
@@ -1,5 +1,35 @@
 export const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
+
+export function quantityWithDivisibility(divisible, quantity_integer) {
+    // return divisible ? (quantity_integer / (10 ** 8)).toFixed(8) : quantity_integer;
+
+    // now done based on string
+    // TODO locale formatting
+    if (divisible) {
+        const quantity_integer_string_length = `${quantity_integer}`.length;
+        let to_return;
+        if (quantity_integer_string_length < 8) {
+            to_return = `${quantity_integer}`;
+            while (to_return.length < 8) {
+                to_return = '0' + to_return;
+            }
+            // is 8, add initial '0.'
+            to_return = '0.' + to_return;
+        }
+        else { // quantity_integer_string_length >= 8
+            const decimals = `${quantity_integer}`.slice(-8);
+            const first_chars_left = `${quantity_integer}`.slice(0, quantity_integer_string_length - 8);
+            to_return = `${first_chars_left ? first_chars_left : '0'}.${decimals}`;
+        }
+        return to_return;
+    }
+    else {
+        return `${quantity_integer}`;
+    }
+}
+
+
 async function getBTCPriceFromBlockchainInfo() {
     const response = await fetch('https://blockchain.info/ticker');
     const json = await response.json();
